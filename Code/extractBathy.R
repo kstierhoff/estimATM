b76059af7a7ab63536ad46a799bb192d59197b9a
Kevin Stierhoff <kevins@ucsc.edu>
2019-08-09 16:05:15 -0700

    Minor updates to estimateBiomass, extractBathy.

--- Code/extractBathy.R
+++ Code/extractBathy.R
@@ -1,15 +1,42 @@
+library(here)
+library(tidyverse)
+library(marmap)
+library(sf)
+
 # Extract bathymetry along each transect ----------------------------------
+get.bathy <- FALSE
+
+# Get bathymetry data across range of nav data (plus/minus one degree lat/long)
+if (get.bathy) {
+  noaa.bathy <- getNOAA.bathy(lon1 = min(transects$Longitude - 1), 
+                             lon2 = max(transects$Longitude + 1),
+                             lat1 = max(transects$Latitude) + 1, 
+                             lat2 = min(transects$Latitude) - 1, 
+                             resolution = 1)
+  # Save bathy results
+  save(bathy_dem, file = here("Data/GIS/bathy_data_1907RL.Rdata"))  
+} else {
+  load(here("Data/GIS/bathy_data_1907RL.Rdata"))
+}
+
+wpts <- read_csv(here("Data/Nav/waypoints_1907RL_scb.csv"))
+
+# extract transect waypoints
+transects <- wpts %>% 
+  mutate(group = paste(transect, region)) 
+
 # Create a df for transect profiles
 tx.prof <- data.frame()
 
 # Extract bathy along each transect and combine
 for (i in unique(transects$group)) {
   tx.tmp <- filter(transects, group == i)
-  tmp <- get.transect(bathy_dem, tx.tmp$Longitude[1], tx.tmp$Latitude[1],
-                      tx.tmp$Longitude[nrow(tx.tmp)], tx.tmp$Latitude[nrow(tx.tmp)],
+  
+  tmp <- get.transect(noaa.bathy, tx.tmp$long[1], tx.tmp$lat[1],
+                      tx.tmp$long[nrow(tx.tmp)], tx.tmp$lat[nrow(tx.tmp)],
                       distance = TRUE) %>% 
-    mutate(transect = unique(tx.tmp$Transect),
-           type = unique(tx.tmp$Type)) %>% 
+    mutate(transect = unique(tx.tmp$transect),
+           region = unique(tx.tmp$region)) %>% 
     filter(depth < 0)
   
   tx.prof <- bind_rows(tx.prof,tmp)
@@ -17,34 +44,50 @@ for (i in unique(transects$group)) {
 
 # Calculate the .99 quantile for each transect type
 type.ecdf <- tx.prof %>% 
-  group_by(type) %>% 
+  group_by(region) %>% 
   summarise(depth.99 = quantile(ecdf(abs(depth)),.99))
 
 # Plot ECDF by type, showing the .99 quantile
 tx.ecdf.plot <- ggplot(tx.prof, aes(abs(depth))) + 
   stat_ecdf() +
   geom_vline(data = type.ecdf, aes(xintercept = depth.99), linetype = "dashed") +
-  facet_wrap(~type, scales = "free") +
-  ylab("ECDF") + xlab("Depth (m)")
+  facet_wrap(~region, scales = "free") +
+  ylab("ECDF") + xlab("Depth (m)") +
+  theme_bw()
 
 # Save plot
-ggsave(tx.ecdf.plot, filename = here("Figs/fig_transect_depth_ecdf.png"))
+ggsave(tx.ecdf.plot, filename = here("Figs/fig_transect_depth_ecdf.png"),
+       height = 6, width = 10)
 
+transect.paths <- transects %>% 
+  st_as_sf(coords = c("long","lat"), crs = 4326) %>% 
+  group_by(transect, region) %>% 
+  summarise(do_union = F) %>% 
+  st_cast("LINESTRING")
 
-# Same analysis, but for WA/OR nearshore transects only -------------------
-# Restrict ECDF to nearshore transects off OR/WA
-type.ecdf.WAOR <- tx.prof %>% 
-  filter(type == "Nearshore", between(transect, 133, 211)) %>% 
-  group_by(type) %>% 
-  summarise(depth.99 = quantile(ecdf(abs(depth)),.99))
+# Read bathy contours shapefile 
+bathy <- st_read(here("Data/GIS/bathy_contours.shp")) %>% 
+  st_transform(4326) %>% 
+  rename(Depth = Contour) %>% 
+  mutate(depth = as.factor(Depth))
 
-# Plot ECDF by type, showing the .99 quantile
-tx.ecdf.WAOR.plot <- tx.prof %>% 
-  filter(type == "Nearshore", between(transect, 133, 211)) %>% 
-  ggplot(aes(abs(depth))) + 
-  stat_ecdf() +
-  geom_vline(data = type.ecdf.WAOR, aes(xintercept = depth.99), linetype = "dashed") +
-  ylab("ECDF") + xlab("Depth (m)")
+mapview::mapview(transect.paths) + 
+  mapview::mapview(bathy, zcol = "depth", legend = FALSE)
 
-# Save plot
-ggsave(tx.ecdf.WAOR.plot, filename = here("Figs/fig_transect_depth_ecdf_WAOR.png"))
+# # Same analysis, but for WA/OR nearshore transects only -------------------
+# # Restrict ECDF to nearshore transects off OR/WA
+# type.ecdf.WAOR <- tx.prof %>% 
+#   filter(type == "Nearshore", between(transect, 133, 211)) %>% 
+#   group_by(type) %>% 
+#   summarise(depth.99 = quantile(ecdf(abs(depth)),.99))
+# 
+# # Plot ECDF by type, showing the .99 quantile
+# tx.ecdf.WAOR.plot <- tx.prof %>% 
+#   filter(type == "Nearshore", between(transect, 133, 211)) %>% 
+#   ggplot(aes(abs(depth))) + 
+#   stat_ecdf() +
+#   geom_vline(data = type.ecdf.WAOR, aes(xintercept = depth.99), linetype = "dashed") +
+#   ylab("ECDF") + xlab("Depth (m)")
+# 
+# # Save plot
+# ggsave(tx.ecdf.WAOR.plot, filename = here("Figs/fig_transect_depth_ecdf_WAOR.png"))
--- Code/map_nearshore_biomass_1907RL.R
+++ Code/map_nearshore_biomass_1907RL.R
@@ -327,94 +327,94 @@ ggsave(nearshore.map.final,
        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
        width  = (map.height*ns.aspect)*2.2, height = map.height*.75)
 
-# Pacific sardine-Southern -------------------------------------------------------------
-i = "Sardinops sagax"
-j = "Southern"
-
-# Get primary and nearshore strata for species i
-strata.nearshore.spp <- filter(strata.nearshore, scientificName == i, stock == j)
-
-strata.primary.spp   <- filter(strata.primary, scientificName == i, stock == j, 
-                               stratum %in% unique(strata.nearshore.spp$stratum))
-
-# Get strata for species i and only the primary survey vessel
-strata.sub <- strata.final %>% 
-  filter(scientificName == i, 
-         stratum %in% unique(strata.nearshore.spp$stratum))
-
-# Set the map boundaries based on the bounding box of the offshore stratum
-ns.lims <- strata.primary.spp %>% 
-  st_transform(crs = crs.proj) %>% 
-  st_bbox()
-
-# Get biomass density in the ROI
-nasc.density.sub <- nasc.density %>% 
-  filter(scientificName == i,
-         density != 0,
-         transect %in% unique(strata.sub$transect)) 
-
-nasc.density.ns.sub <- nasc.density.ns %>% 
-  filter(scientificName == i,
-         density != 0,
-         transect %in% unique(strata.sub$transect)) 
-
-# Select legend objects 
-dens.levels.ns <- sort(unique(nasc.density.sub$bin.level))
-dens.labels.ns <- dens.labels[dens.levels.ns]
-dens.sizes.ns  <- dens.sizes[dens.levels.ns]
-dens.colors.ns <- dens.colors[dens.levels.ns]
-
-# Create polygon around strata bounding box
-inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
-
-# Create inset map
-inset.map <- base.map.bw + 
-  geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
-  coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
-
-# Create main map
-nearshore.map <- base.map + 
-  # Add strata
-  geom_sf(data = strata.primary.spp, aes(colour = factor(stratum)), 
-          fill = NA, linetype = 'dashed', alpha = 0.5) +
-  geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
-          fill = NA, alpha = 0.5) +
-  # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
-  # Plot NASC data
-  geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
-             colour = 'gray50', alpha = 0.75) +
-  geom_point(data = nasc.density.ns.sub, aes(X, Y, size = bin, fill = bin), 
-             shape = 21, alpha = 0.75) +
-  # Configure size and colour scales
-  scale_colour_discrete("Stratum") + 
-  scale_size_manual(name = bquote(atop(Biomass~density, ~'(t'~'nmi'^-2*')')),
-                    values = dens.sizes.ns, labels = dens.labels.ns) +
-  scale_fill_manual(name = bquote(atop(Biomass~density, ~'(t'~'nmi'^-2*')')),
-                    values = dens.colors.ns, labels = dens.labels.ns) +
-  # Configure legend guides
-  guides(fill = guide_legend(), size = guide_legend()) +
-  theme(legend.position      = c(0,0),
-        legend.justification = c(0,0)) +
-  coord_sf(crs = crs.proj,
-           xlim = c(ns.lims["xmin"], ns.lims["xmax"]), 
-           ylim = c(ns.lims["ymin"], ns.lims["ymax"]))
-
-# Get aspect ratio of nearshore map for setting figure dimensions
-ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordinates$limits$y)
-
-# Combine main and inset maps
-nearshore.map.final <- ggdraw() +
-  draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.77, 0.68, 0.3, 0.3) 
-
-# Save combined maps
-ggsave(nearshore.map.final, 
-       filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
-       width  = (map.height*ns.aspect)*.75, height = map.height*.75)
+# # Pacific sardine-Southern -------------------------------------------------------------
+# i = "Sardinops sagax"
+# j = "Southern"
+# 
+# # Get primary and nearshore strata for species i
+# strata.nearshore.spp <- filter(strata.nearshore, scientificName == i, stock == j)
+# 
+# strata.primary.spp   <- filter(strata.primary, scientificName == i, stock == j, 
+#                                stratum %in% unique(strata.nearshore.spp$stratum))
+# 
+# # Get strata for species i and only the primary survey vessel
+# strata.sub <- strata.final %>% 
+#   filter(scientificName == i, 
+#          stratum %in% unique(strata.nearshore.spp$stratum))
+# 
+# # Set the map boundaries based on the bounding box of the offshore stratum
+# ns.lims <- strata.primary.spp %>% 
+#   st_transform(crs = crs.proj) %>% 
+#   st_bbox()
+# 
+# # Get biomass density in the ROI
+# nasc.density.sub <- nasc.density %>% 
+#   filter(scientificName == i,
+#          density != 0,
+#          transect %in% unique(strata.sub$transect)) 
+# 
+# nasc.density.ns.sub <- nasc.density.ns %>% 
+#   filter(scientificName == i,
+#          density != 0,
+#          transect %in% unique(strata.sub$transect)) 
+# 
+# # Select legend objects 
+# dens.levels.ns <- sort(unique(nasc.density.sub$bin.level))
+# dens.labels.ns <- dens.labels[dens.levels.ns]
+# dens.sizes.ns  <- dens.sizes[dens.levels.ns]
+# dens.colors.ns <- dens.colors[dens.levels.ns]
+# 
+# # Create polygon around strata bounding box
+# inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
+# 
+# # Create inset map
+# inset.map <- base.map.bw + 
+#   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
+#   theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+#   coord_sf(crs = crs.proj,
+#            xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
+#            ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+# 
+# # Create main map
+# nearshore.map <- base.map + 
+#   # Add strata
+#   geom_sf(data = strata.primary.spp, aes(colour = factor(stratum)), 
+#           fill = NA, linetype = 'dashed', alpha = 0.5) +
+#   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
+#           fill = NA, alpha = 0.5) +
+#   # Plot vessel track
+#   geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+#   # Plot NASC data
+#   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
+#              colour = 'gray50', alpha = 0.75) +
+#   geom_point(data = nasc.density.ns.sub, aes(X, Y, size = bin, fill = bin), 
+#              shape = 21, alpha = 0.75) +
+#   # Configure size and colour scales
+#   scale_colour_discrete("Stratum") + 
+#   scale_size_manual(name = bquote(atop(Biomass~density, ~'(t'~'nmi'^-2*')')),
+#                     values = dens.sizes.ns, labels = dens.labels.ns) +
+#   scale_fill_manual(name = bquote(atop(Biomass~density, ~'(t'~'nmi'^-2*')')),
+#                     values = dens.colors.ns, labels = dens.labels.ns) +
+#   # Configure legend guides
+#   guides(fill = guide_legend(), size = guide_legend()) +
+#   theme(legend.position      = c(0,0),
+#         legend.justification = c(0,0)) +
+#   coord_sf(crs = crs.proj,
+#            xlim = c(ns.lims["xmin"], ns.lims["xmax"]), 
+#            ylim = c(ns.lims["ymin"], ns.lims["ymax"]))
+# 
+# # Get aspect ratio of nearshore map for setting figure dimensions
+# ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordinates$limits$y)
+# 
+# # Combine main and inset maps
+# nearshore.map.final <- ggdraw() +
+#   draw_plot(nearshore.map) +
+#   draw_plot(inset.map, 0.77, 0.68, 0.3, 0.3) 
+# 
+# # Save combined maps
+# ggsave(nearshore.map.final, 
+#        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
+#        width  = (map.height*ns.aspect)*.75, height = map.height*.75)
 
 # Pacific mackerel-All ---------------------------------------------------------
 i = "Scomber japonicus"
