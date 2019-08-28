a8ec5a2a43667c0128030753d328b97b1583451f
Kevin Stierhoff <kevins@ucsc.edu>
2019-08-28 14:32:25 -0700

    Minor bug fixes to estimateBiomass. Updated nearshore extrapolation code and added plots to estimateBiomass output.

--- Code/map_nearshore_biomass_1907RL.R
+++ Code/map_nearshore_biomass_1907RL.R
@@ -33,7 +33,7 @@ nasc.map.ns <- base.map +
                         stock == stock.ns, stratum == strata.ns),
           fill = NA, size = 0.5, colour = 'red') +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -100,13 +100,20 @@ dens.colors.ns <- dens.colors[dens.levels.ns]
 # Create polygon around strata bounding box
 inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 
+# Define map boundaries using inset map extent and sampling domain
+inset.map.bounds <- st_as_sfc(st_bbox(inset.extent)) %>% 
+  st_union(st_as_sfc(st_bbox(transects.sf))) %>% 
+  st_transform(crs = crs.proj) %>%
+  st_bbox() 
+
 # Create inset map
 inset.map <- base.map.bw + 
   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+  labs(x = NULL, y = NULL) +
   coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+           xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+           ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 
 # Create main map
 nearshore.map <- base.map + 
@@ -116,7 +123,7 @@ nearshore.map <- base.map +
   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
           fill = NA, alpha = 0.5) +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -142,12 +149,12 @@ ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordin
 # Combine main and inset maps
 nearshore.map.final <- ggdraw() +
   draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.31, 0.72, 0.25, 0.25) 
+  draw_plot(inset.map, 0.29, 0.72, 0.25, 0.25) 
 
 # Save combined maps
 ggsave(nearshore.map.final, 
        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
-       width  = (map.height*ns.aspect)*2, height = map.height*.75)
+       width  = (map.height*ns.aspect)*1.6, height = map.height*.75)
 
 # Northern anchovy-Central -------------------------------------------------------------
 i = "Engraulis mordax"
@@ -189,13 +196,20 @@ dens.colors.ns <- dens.colors[dens.levels.ns]
 # Create polygon around strata bounding box
 inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 
+# Define map boundaries using inset map extent and sampling domain
+inset.map.bounds <- st_as_sfc(st_bbox(inset.extent)) %>% 
+  st_union(st_as_sfc(st_bbox(transects.sf))) %>% 
+  st_transform(crs = crs.proj) %>%
+  st_bbox() 
+
 # Create inset map
 inset.map <- base.map.bw + 
   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+  labs(x = NULL, y = NULL) +
   coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+           xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+           ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 
 # Create main map
 nearshore.map <- base.map + 
@@ -205,7 +219,7 @@ nearshore.map <- base.map +
   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
           fill = NA, alpha = 0.5) +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -231,7 +245,7 @@ ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordin
 # Combine main and inset maps
 nearshore.map.final <- ggdraw() +
   draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.69, 0.62, 0.35, 0.35) 
+  draw_plot(inset.map, 0.67, 0.62, 0.35, 0.35) 
 
 # Save combined maps
 ggsave(nearshore.map.final, 
@@ -278,13 +292,20 @@ dens.colors.ns <- dens.colors[dens.levels.ns]
 # Create polygon around strata bounding box
 inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 
+# Define map boundaries using inset map extent and sampling domain
+inset.map.bounds <- st_as_sfc(st_bbox(inset.extent)) %>% 
+  st_union(st_as_sfc(st_bbox(transects.sf))) %>% 
+  st_transform(crs = crs.proj) %>%
+  st_bbox() 
+
 # Create inset map
 inset.map <- base.map.bw + 
   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+  labs(x = NULL, y = NULL) +
   coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+           xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+           ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 
 # Create main map
 nearshore.map <- base.map + 
@@ -294,7 +315,7 @@ nearshore.map <- base.map +
   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
           fill = NA, alpha = 0.5) +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -320,12 +341,12 @@ ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordin
 # Combine main and inset maps
 nearshore.map.final <- ggdraw() +
   draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.37, 0.78, 0.2, 0.2) 
+  draw_plot(inset.map, 0.57, 0.78, 0.2, 0.2) 
 
 # Save combined maps
 ggsave(nearshore.map.final, 
        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
-       width  = (map.height*ns.aspect)*2.2, height = map.height*.75)
+       width  = (map.height*ns.aspect)*1.5, height = map.height*.75)
 
 # # Pacific sardine-Southern -------------------------------------------------------------
 # i = "Sardinops sagax"
@@ -334,31 +355,31 @@ ggsave(nearshore.map.final,
 # # Get primary and nearshore strata for species i
 # strata.nearshore.spp <- filter(strata.nearshore, scientificName == i, stock == j)
 # 
-# strata.primary.spp   <- filter(strata.primary, scientificName == i, stock == j, 
+# strata.primary.spp   <- filter(strata.primary, scientificName == i, stock == j,
 #                                stratum %in% unique(strata.nearshore.spp$stratum))
 # 
 # # Get strata for species i and only the primary survey vessel
-# strata.sub <- strata.final %>% 
-#   filter(scientificName == i, 
+# strata.sub <- strata.final %>%
+#   filter(scientificName == i,
 #          stratum %in% unique(strata.nearshore.spp$stratum))
 # 
 # # Set the map boundaries based on the bounding box of the offshore stratum
-# ns.lims <- strata.primary.spp %>% 
-#   st_transform(crs = crs.proj) %>% 
+# ns.lims <- strata.primary.spp %>%
+#   st_transform(crs = crs.proj) %>%
 #   st_bbox()
 # 
 # # Get biomass density in the ROI
-# nasc.density.sub <- nasc.density %>% 
+# nasc.density.sub <- nasc.density %>%
 #   filter(scientificName == i,
 #          density != 0,
-#          transect %in% unique(strata.sub$transect)) 
+#          transect %in% unique(strata.sub$transect))
 # 
-# nasc.density.ns.sub <- nasc.density.ns %>% 
+# nasc.density.ns.sub <- nasc.density.ns %>%
 #   filter(scientificName == i,
 #          density != 0,
-#          transect %in% unique(strata.sub$transect)) 
+#          transect %in% unique(strata.sub$transect))
 # 
-# # Select legend objects 
+# # Select legend objects
 # dens.levels.ns <- sort(unique(nasc.density.sub$bin.level))
 # dens.labels.ns <- dens.labels[dens.levels.ns]
 # dens.sizes.ns  <- dens.sizes[dens.levels.ns]
@@ -368,29 +389,31 @@ ggsave(nearshore.map.final,
 # inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 # 
 # # Create inset map
+# Create inset map
 # inset.map <- base.map.bw + 
 #   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-#   theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+#   theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+#   labs(x = NULL, y = NULL) +
 #   coord_sf(crs = crs.proj,
-#            xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-#            ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+#            xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+#            ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 # 
 # # Create main map
-# nearshore.map <- base.map + 
+# nearshore.map <- base.map +
 #   # Add strata
-#   geom_sf(data = strata.primary.spp, aes(colour = factor(stratum)), 
+#   geom_sf(data = strata.primary.spp, aes(colour = factor(stratum)),
 #           fill = NA, linetype = 'dashed', alpha = 0.5) +
-#   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
+#   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)),
 #           fill = NA, alpha = 0.5) +
 #   # Plot vessel track
-#   geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+#   geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) +
 #   # Plot NASC data
-#   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
+#   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin),
 #              colour = 'gray50', alpha = 0.75) +
-#   geom_point(data = nasc.density.ns.sub, aes(X, Y, size = bin, fill = bin), 
+#   geom_point(data = nasc.density.ns.sub, aes(X, Y, size = bin, fill = bin),
 #              shape = 21, alpha = 0.75) +
 #   # Configure size and colour scales
-#   scale_colour_discrete("Stratum") + 
+#   scale_colour_discrete("Stratum") +
 #   scale_size_manual(name = bquote(atop(Biomass~density, ~'(t'~'nmi'^-2*')')),
 #                     values = dens.sizes.ns, labels = dens.labels.ns) +
 #   scale_fill_manual(name = bquote(atop(Biomass~density, ~'(t'~'nmi'^-2*')')),
@@ -400,7 +423,7 @@ ggsave(nearshore.map.final,
 #   theme(legend.position      = c(0,0),
 #         legend.justification = c(0,0)) +
 #   coord_sf(crs = crs.proj,
-#            xlim = c(ns.lims["xmin"], ns.lims["xmax"]), 
+#            xlim = c(ns.lims["xmin"], ns.lims["xmax"]),
 #            ylim = c(ns.lims["ymin"], ns.lims["ymax"]))
 # 
 # # Get aspect ratio of nearshore map for setting figure dimensions
@@ -409,10 +432,10 @@ ggsave(nearshore.map.final,
 # # Combine main and inset maps
 # nearshore.map.final <- ggdraw() +
 #   draw_plot(nearshore.map) +
-#   draw_plot(inset.map, 0.77, 0.68, 0.3, 0.3) 
+#   draw_plot(inset.map, 0.77, 0.68, 0.3, 0.3)
 # 
 # # Save combined maps
-# ggsave(nearshore.map.final, 
+# ggsave(nearshore.map.final,
 #        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
 #        width  = (map.height*ns.aspect)*.75, height = map.height*.75)
 
@@ -456,13 +479,20 @@ dens.colors.ns <- dens.colors[dens.levels.ns]
 # Create polygon around strata bounding box
 inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 
+# Define map boundaries using inset map extent and sampling domain
+inset.map.bounds <- st_as_sfc(st_bbox(inset.extent)) %>% 
+  st_union(st_as_sfc(st_bbox(transects.sf))) %>% 
+  st_transform(crs = crs.proj) %>%
+  st_bbox() 
+
 # Create inset map
 inset.map <- base.map.bw + 
   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+  labs(x = NULL, y = NULL) +
   coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+           xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+           ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 
 # Create main map
 nearshore.map <- base.map + 
@@ -472,7 +502,7 @@ nearshore.map <- base.map +
   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
           fill = NA, alpha = 0.5) +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -486,21 +516,20 @@ nearshore.map <- base.map +
                     values = dens.colors.ns, labels = dens.labels.ns) +
   # Configure legend guides
   guides(fill = guide_legend(), size = guide_legend()) +
-  theme(legend.position      = c(0,0),
-        legend.justification = c(0,0)) +
+  theme(legend.position      = c(1,0.5),
+        legend.justification = c(0,0.5)) +
   coord_sf(crs = crs.proj,
            xlim = c(ns.lims["xmin"], ns.lims["xmax"]), 
            ylim = c(ns.lims["ymin"], ns.lims["ymax"]))
 
-# Combine main and inset maps
 nearshore.map.final <- ggdraw() +
   draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.65, 0.71, 0.25, 0.25) 
+  draw_plot(inset.map, 0.38, 0.78, 0.2, 0.2) 
 
 # Save combined maps
 ggsave(nearshore.map.final, 
        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
-       width  = (map.height*ns.aspect)*.25, height = map.height*.75)
+       width  = (map.height*ns.aspect)*1.5, height = map.height*.75)
 
 # Jack mackerel-All ---------------------------------------------------------
 i = "Trachurus symmetricus"
@@ -542,13 +571,20 @@ dens.colors.ns <- dens.colors[dens.levels.ns]
 # Create polygon around strata bounding box
 inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 
+# Define map boundaries using inset map extent and sampling domain
+inset.map.bounds <- st_as_sfc(st_bbox(inset.extent)) %>% 
+  st_union(st_as_sfc(st_bbox(transects.sf))) %>% 
+  st_transform(crs = crs.proj) %>%
+  st_bbox() 
+
 # Create inset map
 inset.map <- base.map.bw + 
   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+  labs(x = NULL, y = NULL) +
   coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+           xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+           ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 
 # Create main map
 nearshore.map <- base.map + 
@@ -558,7 +594,7 @@ nearshore.map <- base.map +
   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
           fill = NA, alpha = 0.5) +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -584,12 +620,12 @@ ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordin
 # Combine main and inset maps
 nearshore.map.final <- ggdraw() +
   draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.55, 0.72, 0.25, 0.25) 
+  draw_plot(inset.map, 0.39, 0.78, 0.20, 0.20) 
 
 # Save combined maps
 ggsave(nearshore.map.final, 
        filename = paste(here("Figs/fig_biomass_dens_nearshore_"), i, "-", j, ".png",sep = ""),
-       width  = (map.height*ns.aspect)*1.5, height = map.height*.75)
+       width  = (map.height*ns.aspect)*2.2, height = map.height*.75)
 
 # Pacific herring-All ---------------------------------------------------------
 i = "Clupea pallasii"
@@ -631,13 +667,20 @@ dens.colors.ns <- dens.colors[dens.levels.ns]
 # Create polygon around strata bounding box
 inset.extent <- st_as_sfc(st_bbox(strata.primary.spp))
 
+# Define map boundaries using inset map extent and sampling domain
+inset.map.bounds <- st_as_sfc(st_bbox(inset.extent)) %>% 
+  st_union(st_as_sfc(st_bbox(transects.sf))) %>% 
+  st_transform(crs = crs.proj) %>%
+  st_bbox() 
+
 # Create inset map
 inset.map <- base.map.bw + 
   geom_sf(data = inset.extent, colour = 'red', fill = NA, size = 1) +
-  theme(plot.margin = unit(c(0, 0, 0, 0), "null")) +
+  theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
+  labs(x = NULL, y = NULL) +
   coord_sf(crs = crs.proj,
-           xlim = c(map.bounds["xmin"], map.bounds["xmax"]), 
-           ylim = c(map.bounds["ymin"], map.bounds["ymax"]))
+           xlim = c(inset.map.bounds["xmin"], inset.map.bounds["xmax"]), 
+           ylim = c(inset.map.bounds["ymin"], inset.map.bounds["ymax"]))
 
 # Create main map
 nearshore.map <- base.map + 
@@ -647,7 +690,7 @@ nearshore.map <- base.map +
   geom_sf(data = strata.nearshore.spp, aes(colour = factor(stratum)), 
           fill = NA, alpha = 0.5) +
   # Plot vessel track
-  geom_sf(data = nav.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
+  geom_sf(data = nav.paths.sf, colour = 'gray50', size = 0.25, alpha = 0.5) + 
   # Plot NASC data
   geom_point(data = nasc.density.sub, aes(X, Y, size = bin, fill = bin), 
              colour = 'gray50', alpha = 0.75) +
@@ -673,7 +716,7 @@ ns.aspect <- diff(nearshore.map$coordinates$limits$x)/diff(nearshore.map$coordin
 # Combine main and inset maps
 nearshore.map.final <- ggdraw() +
   draw_plot(nearshore.map) +
-  draw_plot(inset.map, 0.73, 0.725, 0.225, 0.225) 
+  draw_plot(inset.map, 0.73, 0.715, 0.225, 0.225) 
 
 # Save combined maps
 ggsave(nearshore.map.final, 
