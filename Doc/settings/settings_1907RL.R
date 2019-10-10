7b3fa829952769f9b9fda2c23226829b93228c28
Kevin Stierhoff <kevins@ucsc.edu>
2019-10-10 12:27:34 -0700

    Revisions to reportSurvey after Josiah's edits.

--- Doc/settings/settings_1907RL.R
+++ Doc/settings/settings_1907RL.R
@@ -425,7 +425,7 @@ raw.log.range <- 1000  # depth of ER60 logging (m)
 
 # Echoview settings
 er60.version  <- "v2.4.3" # ER60 version
-ek80.version  <- "v1.11" # EK80 version
+ek80.version  <- "v1.12.2" # EK80 version
 ev.version    <- "v10.0" # Echoview version
 int.start        <-    5  # Integration start line depth (m)
 int.stop         <- 1000  # Integration start line depth (m)
