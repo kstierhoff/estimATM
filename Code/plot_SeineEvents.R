0f03cc92195ddef8576b2207ed80db06c1b0cc38
Kevin Stierhoff <kevins@ucsc.edu>
2020-02-13 15:00:26 -0800

    Minor updates to nasc_nearshore.

--- Code/plot_SeineEvents.R
+++ Code/plot_SeineEvents.R
@@ -7,3 +7,16 @@ pacman::p_load(tidyverse,lubridate,here)
 # Load log book data
 logs <- read_csv(here("Data/Seine/lbc_logs.csv")) %>% 
   mutate(datetime = with_tz(mdy_hms(paste(date, as.character(time)), tz = "America/Los_Angeles"), tzone = "UTC"))
+
+# Load backscatter data
+load(here("Output/nasc_nearshore_final.Rdata"))
+
+# Filter and format
+nav <- nasc.nearshore %>% 
+  filter(vessel.name == "LBC") %>% 
+  select(datetime, long, lat, transect)
+
+# Plot nav
+ggplot(nav, aes(long, lat, group = transect)) + 
+  geom_path() + 
+  coord_map()
