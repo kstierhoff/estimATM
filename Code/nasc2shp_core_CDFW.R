5ebf6ae8ea9620df536bbca39bd6f570c0e50db2
Kevin Stierhoff <kevins@ucsc.edu>
2022-03-30 10:01:57 -0700

    Updated exporting of nearshore and core transects for CDFW.

--- Code/nasc2shp_core_CDFW.R
+++ Code/nasc2shp_core_CDFW.R
@@ -7,13 +7,13 @@ library(mapview)
 
 fs::dir_create(here::here("Output/CDFW"))
 
-load(here::here("Data/Backscatter/nasc_nearshore.Rdata"))
+load(here::here("Data/Backscatter/nasc_all.Rdata"))
 # load(here::here("C:/KLS/CODE/R_packages/estimATM/2103RL/Data/Backscatter/nasc_nearshore.Rdata"))
 # load(here::here("C:/KLS/CODE/R_packages/estimATM/2107RL/Data/Backscatter/nasc_nearshore.Rdata"))
 
-nasc.sf <- st_as_sf(nasc.nearshore, coords = c("long","lat"), crs = 4326) %>% 
+nasc.sf <- st_as_sf(nasc, coords = c("long","lat"), crs = 4326) %>% 
   mutate(date_group = as.factor((date(datetime)))) %>% 
-  filter(vessel.name == "LBC")
+  filter(vessel.name == "RL")
 
 nasc.summ <- nasc.sf %>% 
   group_by(transect.name) %>% 
@@ -21,7 +21,7 @@ nasc.summ <- nasc.sf %>%
   st_set_geometry(NULL)
 
 # Convert to lines, by date
-nearshore.lines <- nasc.sf %>% 
+core.lines <- nasc.sf %>% 
   group_by(vessel.name, transect.name) %>% 
   summarise(do_union = F) %>% 
   st_cast("LINESTRING") %>% 
@@ -31,17 +31,16 @@ nearshore.lines <- nasc.sf %>%
          transect = transect.name,
          dist_nmi = distance_nmi)
 
-nearshore.lines.date <- nasc.sf %>% 
+core.lines.date <- nasc.sf %>% 
   group_by(date_group) %>% 
   summarise(do_union = F) %>% 
   st_cast("LINESTRING") %>% 
-  mutate(distance_nmi = as.numeric(st_length(.)*0.000539957)) %>% 
-  rename(dist_nmi = distance_nmi)
+  mutate(dist_nmi = as.numeric(st_length(.)*0.000539957))
 
-st_write(nearshore.lines, "Output/CDFW/nav_nearshore_CDFW_lines.shp",
+st_write(core.lines, "Output/CDFW/nav_core_CDFW_lines.shp",
          delete_layer = TRUE)
 
-st_write(nearshore.lines.date, "Output/CDFW/nav_nearshore_CDFW_lines_date.shp",
+st_write(core.lines.date, "Output/CDFW/nav_core_CDFW_lines_date.shp",
          delete_layer = TRUE)
 
 # mapview(nearshore.lines)
