6cc91b84fcc7a69509ae7fcb232ce3e4d4e6a610
Kevin Stierhoff <kevins@ucsc.edu>
2022-07-26 17:20:25 -0400

    Added script for creating gps.csv files from SCS (scs2gps.R).

---
+++ Code/scs2gps.R
@@ -0,0 +1,28 @@
+# Create gps.csv file for Echoview processing from SCS data
+
+pacman::p_load(tidyverse, here, lubridate)
+pacman::p_load_gh("kstierhoff/surveyR")
+
+# Set start and end date to reduce file size (makes matching faster, I think)
+start_date <- ymd("2022-07-25")
+end_date <- ymd("2022-07-30")
+# end_date <- ymd(Sys.Date())
+
+nav.csv <- read_csv(here("Data/SCS/2207RL_GPS.csv")) %>% 
+  select(GPS_date = Date, GPS_time = Time, latitude = "GP170-Lat",longitude = "GP170-Lon") %>% 
+  mutate(latitude = scs2dd(latitude),
+         longitude = scs2dd(longitude),
+         GPS_date = mdy(GPS_date)) %>% 
+  filter(between(GPS_date, start_date, end_date))
+
+# Create gps.csv file from nav to replace missing data in Echoview
+nav.gps <- nav.csv %>% 
+  mutate(GPS_date = format(GPS_date, format = "%F"),
+         GPS_time = format(GPS_time, format = "%T")) %>% 
+  select(GPS_date, GPS_time, latitude, longitude)
+
+# Write file
+write_csv(nav.gps, here("Output/nav.gps.csv"))
+
+# Plot results
+ggplot(nav.csv, aes(longitude, latitude)) + geom_path()
