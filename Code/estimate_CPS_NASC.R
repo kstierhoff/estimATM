b1013522db8fcc374225707ad665af8d501d82c5
Kevin Stierhoff <kevins@ucsc.edu>
2022-09-21 19:12:50 -0700

    Added the ability to open the exported echogram from extract_cps_nasc.R.

--- Code/estimate_CPS_NASC.R
+++ Code/estimate_CPS_NASC.R
@@ -1,4 +1,4 @@
-estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200, transparency = 0.2,
+estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL, pattern.img = NULL, max.range = 200, transparency = 0.2,
                               expand.left = F, expand.right = F, expansion = 2, suffix = "_nasc_cps", 
                               jpeg = T, root = 1.5, scaling = 1) {
   exponent <- 1
@@ -14,7 +14,7 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   acoustic.file.list <- list.files(path = path.input, pattern = pattern, recursive = F)
   print(acoustic.file.list)
   
-  print("Type the order of the file you want to integrate")
+  print("Type the order of the file you want to integrate:")
   test.1 <- scan(n = 1)
   
   for (i in acoustic.file.list[test.1]) {
@@ -62,18 +62,30 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
                         fileEncoding = "UTF-8-BOM"
   )
   
+  if (!is.null(path.img)) {
+    # List the exported images
+    img.file.list <- list.files(path = path.img, pattern = pattern.img, recursive = F)
+    print(img.file.list)
+    
+    print("Type the order of the exported echogram to open:")
+    img.1 <- scan(n = 1)
+    
+    # Open the selected image
+    shell.exec(file.path(path.img, img.file.list[img.1]))
+  }
+  
   # plot
   if (jpeg == T) {
     jpeg(paste(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_original", ".jpeg", sep = ""), width = 35, height = 25, units = "cm", res = 200)
     
-    plot(temporary$Interval^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Interval", xaxt = "n") # this is better because it follows the images
-    axis(side = 1, at = seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5), lab = floor(seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5)^(1 / exponent)))
+    plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n") # this is better because it follows the images
+    axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
     
     
-    lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
+    lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
     
-    points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-    points((temporary$Interval[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+    points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+    points((temporary$Dist_M[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
     
     abline(h = 50, col = "gray", lwd = 0.5)
     abline(h = 100, col = "gray", lwd = 0.5)
@@ -86,15 +98,15 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   }
   
   
-  x11()
-  plot(temporary$Interval^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Interval", xaxt = "n") # this is better because it follows the images
-  axis(side = 1, at = seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5), lab = floor(seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5)^(1 / exponent)))
+  x11(width = 1200, height = 500)
+  plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n") # this is better because it follows the images
+  axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
   
   
-  lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
+  lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
   
-  points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-  points((temporary$Interval[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+  points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+  points((temporary$Dist_M[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
   
   abline(h = 50, col = "gray", lwd = 0.5)
   abline(h = 100, col = "gray", lwd = 0.5)
@@ -112,8 +124,8 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   while (test.3 == 0) {
     print("Select the bottom integration range by clicking on the desired locations - stop by right-clicking")
     new.lines.bottom <- locator(type = "l", lty = 2, lwd = 0.5)
-    b <- approx(new.lines.bottom$x, new.lines.bottom$y, xout = temporary$Interval^exponent, rule = 2, method = "linear")$y
-    points(temporary$Interval^exponent, b, cex = 1, col = col.line, pch = 19)
+    b <- approx(new.lines.bottom$x, new.lines.bottom$y, xout = temporary$Dist_M^exponent, rule = 2, method = "linear")$y
+    points(temporary$Dist_M^exponent, b, cex = 1, col = col.line, pch = 19)
     col.line <- col.line + 1
     print("Does it look good now? 1 = Yes; 0 = No")
     test.3 <- scan(n = 1)
@@ -132,8 +144,8 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
     while (test.3 == 0) {
       print("Select the top integration range by clicking on the desired locations - stop by right-clicking")
       new.lines.top <- locator(type = "l", lty = 2, lwd = 0.5)
-      b <- approx(new.lines.top$x, new.lines.top$y, xout = temporary$Interval^exponent, rule = 2, method = "linear")$y
-      points(temporary$Interval^exponent, b, cex = 1, col = col.line, pch = 19)
+      b <- approx(new.lines.top$x, new.lines.top$y, xout = temporary$Dist_M^exponent, rule = 2, method = "linear")$y
+      points(temporary$Dist_M^exponent, b, cex = 1, col = col.line, pch = 19)
       col.line <- col.line + 1
       print("Does it look good now? 1 = Yes; 0 = No")
       test.3 <- scan(n = 1)
@@ -150,14 +162,14 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   temporary$cps.nasc[temporary$Depth_mean < temporary$top.habitat | temporary$Depth_mean > temporary$bottom.habitat] <- 0
   
   # plot
-  x11()
-  plot(temporary$Interval^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Interval", xaxt = "n")
-  axis(side = 1, at = seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5), lab = floor(seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5)^(1 / exponent)))
+  x11(width = 1200, height = 500)
+  plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n")
+  axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
   
-  lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
+  lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
   
-  points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-  points((temporary$Interval[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+  points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+  points((temporary$Dist_M[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
   
   abline(h = 50, col = "gray", lwd = 0.5)
   abline(h = 100, col = "gray", lwd = 0.5)
@@ -171,18 +183,18 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   if (test.4 == 1) {
     polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), c(new.lines.top$y, rev(new.lines.bottom$y)), col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
   }
-  points(temporary$Interval^exponent, temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
-  points(temporary$Interval^exponent, temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
+  points(temporary$Dist_M^exponent, temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
+  points(temporary$Dist_M^exponent, temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
   
   if (jpeg == T) {
     jpeg(paste(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_clean", ".jpeg", sep = ""), width = 35, height = 25, units = "cm", res = 200)
-    plot(temporary$Interval^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Interval", xaxt = "n")
-    axis(side = 1, at = seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5), lab = floor(seq(min(temporary$Interval^exponent), max(temporary$Interval^exponent), l = 5)^(1 / exponent)))
+    plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n")
+    axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
     
-    lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
+    lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
     
-    points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-    points((temporary$Interval[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+    points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+    points((temporary$Dist_M[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
     
     abline(h = 50, col = "gray", lwd = 0.5)
     abline(h = 100, col = "gray", lwd = 0.5)
@@ -195,8 +207,8 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
     if (test.4 == 1) {
       polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), c(new.lines.top$y, rev(new.lines.bottom$y)), col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
     }
-    points(temporary$Interval^exponent, temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
-    points(temporary$Interval^exponent, temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
+    points(temporary$Dist_M^exponent, temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
+    points(temporary$Dist_M^exponent, temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
     dev.off()
   }
   
--- Code/extract_CPS_NASC.R
+++ Code/extract_CPS_NASC.R
@@ -15,18 +15,14 @@ source(here::here("Code/estimate_CPS_NASC.R"))
 # Configure input and output paths
 path.in  <- here("Data/Backscatter/RL")
 path.out <- here("Data/Backscatter/RL")
-# path.in  <- "C:/SURVEY/2107RL/PROCESSED/EV/CSV/SAILDRONE/SD1059"
-# path.out <- "C:/SURVEY/2107RL/PROCESSED/EV/CSV/SAILDRONE/SD1059"
-# path.in  <- "C:/Users/josiah.renfree/Desktop/2107RL_SD_Reprocessing/EV/CSV/SD1059/CPSFilter_-13dB"
-# path.out <- "C:/Users/josiah.renfree/Desktop/2107RL_SD_Reprocessing/EV/CSV/SD1059/CPSFilter_-13dB/POST-PROCESSED"
-
-# List CSV files (this is just for viewing the files; it's not used in the code)
-list.files(path = path.in, pattern = "CPS-Final 38 kHz CPS.csv", recursive = FALSE)
+path.img <- "C:/SURVEY/2207RL/PROCESSED/EV/Exported_Images"
 
 # Run the function to estimate CPS backscatter
 estimate.cps.nasc(path.input = path.in, 
                   pattern = "CPS-Final 38 kHz CPS.csv", 
                   path.output = path.out, 
+                  path.img = path.img,
+                  pattern.img = "CPS-38 kHz CPS for Image Export.png",
                   expand.right = T,
                   expand.left = F,
                   expansion = 2, max.range = 350, root = 2,  scaling = 0.5)
