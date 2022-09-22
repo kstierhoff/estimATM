550042a0fcb3ff082917088f19f560aced507da4
Kevin Stierhoff <kevins@ucsc.edu>
2022-09-21 21:36:18 -0700

    Refined the estimate_cps_nasc.r and extract_cps_nasc.r scripts.

--- Code/estimate_CPS_NASC.R
+++ Code/estimate_CPS_NASC.R
@@ -1,16 +1,17 @@
-estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL, pattern.img = NULL, max.range = 200, transparency = 0.2,
-                              expand.left = F, expand.right = F, expansion = 2, suffix = "_nasc_cps", 
-                              jpeg = T, root = 1.5, scaling = 1) {
+estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL, pattern.img = NULL, max.range = 200, 
+                              transparency = 0.2, expand.left = FALSE, expand.right = FALSE, expansion = 2, 
+                              suffix, jpeg, root = 1.5, scaling = 1) {
   exponent <- 1
   
-  if (expand.left == T) {
+  if (expand.left) {
     exponent <- 1 / expansion
   }
   
-  if (expand.right == T) {
+  if (expand.right) {
     exponent <- expansion
   }
   
+  # List CSV files for processing
   acoustic.file.list <- list.files(path = path.input, pattern = pattern, recursive = F)
   print(acoustic.file.list)
   
@@ -22,10 +23,8 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
     
     processed.file <- list.files(
       path = path.input,
-      pattern =
-        paste(substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_nasc_cps", sep = ""),
-      recursive = F
-    )
+      pattern = paste0(substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_nasc_cps"),
+      recursive = FALSE)
     
     if (length(processed.file) > 0) {
       print(paste("There is already a processed file with the name", processed.file))
@@ -42,10 +41,8 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
       print(acoustic.file.list[test.1])
       processed.file <- list.files(
         path = path.input,
-        pattern =
-          paste(substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_nasc_cps", sep = ""),
-        recursive = F
-      )
+        pattern = paste0(substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_nasc_cps"),
+        recursive = FALSE)
       
       if (length(processed.file) > 0) {
         print(paste("There is already a processed file with the name", processed.file))
@@ -54,13 +51,9 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
       test.2 <- scan(n = 1)
     }
   }
-  ## so far it is working
-  
-  print(paste("Expansion =", exponent))
-  
-  temporary <- read.csv(paste(path.input, "\\", acoustic.file.list[test.1], sep = ""),
-                        fileEncoding = "UTF-8-BOM"
-  )
+  # Read the CSV file
+  temporary <- read.csv(paste0(path.input, "\\", acoustic.file.list[test.1]),
+                        fileEncoding = "UTF-8-BOM")
   
   if (!is.null(path.img)) {
     # List the exported images
@@ -75,38 +68,68 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
   }
   
   # plot
-  if (jpeg == T) {
-    jpeg(paste(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_original", ".jpeg", sep = ""), width = 35, height = 25, units = "cm", res = 200)
+  if (jpeg) {
+    jpeg(paste0(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_original.jpeg"), 
+         width = 35, height = 25, units = "cm", res = 200)
     
-    plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n") # this is better because it follows the images
-    axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
+    plot(temporary$Dist_M^exponent, 
+         temporary$Depth_mean, 
+         ylim = c(max.range, 0), type = "n", 
+         main = acoustic.file.list[test.1], 
+         ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n") # this is better because it follows the images
+    axis(side = 1, at = seq(min(temporary$Dist_M^exponent), 
+                            max(temporary$Dist_M^exponent), l = 5), 
+         lab = floor(seq(min(temporary$Dist_M^exponent), 
+                         max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
     
     
-    lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
+    lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), 
+          unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
     
-    points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-    points((temporary$Dist_M[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+    points(temporary$Dist_M^exponent, 
+           temporary$Depth_mean, 
+           cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, 
+           pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+    points((temporary$Dist_M[temporary$NASC / 50000 * 30 > 1])^exponent, 
+           temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], 
+           pch = ".", cex = 2, col = "red")
     
-    abline(h = 50, col = "gray", lwd = 0.5)
+    abline(h = 50,  col = "gray", lwd = 0.5)
     abline(h = 100, col = "gray", lwd = 0.5)
     abline(h = 150, col = "gray", lwd = 0.5)
     abline(h = 200, col = "gray", lwd = 0.5)
     abline(h = 250, col = "gray", lwd = 0.5)
     abline(h = 300, col = "gray", lwd = 0.5)
-    abline(h = 0, col = "gray", lwd = 0.5)
-    dev.off()
+    abline(h = 0,   col = "gray", lwd = 0.5)
+    graphics.off()
   }
   
   
-  x11(width = 1200, height = 500)
-  plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n") # this is better because it follows the images
-  axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
+  x11(width = 1200, height = 400)
+  plot(temporary$Dist_M^exponent, 
+       temporary$Depth_mean, 
+       ylim = c(max.range, 0), type = "n", 
+       main = acoustic.file.list[test.1], 
+       ylab = "Depth (m)", xlab = "Dist_M", 
+       xaxt = "n") # this is better because it follows the images
+  axis(side = 1, at = seq(min(temporary$Dist_M^exponent), 
+                          max(temporary$Dist_M^exponent), l = 5), 
+       lab = floor(seq(min(temporary$Dist_M^exponent), 
+                       max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
   
   
-  lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
+  lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), 
+        unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), 
+        col = 1, lwd = 2)
   
-  points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-  points((temporary$Dist_M[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+  points(temporary$Dist_M^exponent, 
+         temporary$Depth_mean, 
+         cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, 
+         pch = 19, 
+         col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+  
+  points((temporary$Dist_M[temporary$NASC / 50000 * 30 > 1])^exponent, 
+         temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
   
   abline(h = 50, col = "gray", lwd = 0.5)
   abline(h = 100, col = "gray", lwd = 0.5)
@@ -117,14 +140,15 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
   abline(h = 0, col = "gray", lwd = 0.5)
   
   # Manual drawing only
-  
   test.3 <- 0
   col.line <- 3
   
   while (test.3 == 0) {
     print("Select the bottom integration range by clicking on the desired locations - stop by right-clicking")
     new.lines.bottom <- locator(type = "l", lty = 2, lwd = 0.5)
-    b <- approx(new.lines.bottom$x, new.lines.bottom$y, xout = temporary$Dist_M^exponent, rule = 2, method = "linear")$y
+    b <- approx(new.lines.bottom$x, 
+                new.lines.bottom$y, 
+                xout = temporary$Dist_M^exponent, rule = 2, method = "linear")$y
     points(temporary$Dist_M^exponent, b, cex = 1, col = col.line, pch = 19)
     col.line <- col.line + 1
     print("Does it look good now? 1 = Yes; 0 = No")
@@ -158,58 +182,93 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
     temporary$top.habitat <- 0
   }
   
+  # Define CPS nasc
   temporary$cps.nasc <- temporary$NASC
   temporary$cps.nasc[temporary$Depth_mean < temporary$top.habitat | temporary$Depth_mean > temporary$bottom.habitat] <- 0
   
   # plot
-  x11(width = 1200, height = 500)
-  plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n")
-  axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
+  x11(width = 1200, height = 400)
+  plot(temporary$Dist_M^exponent, 
+       temporary$Depth_mean, 
+       ylim = c(max.range, 0), type = "n", 
+       main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n")
+  axis(side = 1, at = seq(min(temporary$Dist_M^exponent), 
+                          max(temporary$Dist_M^exponent), l = 5), 
+       lab = floor(seq(min(temporary$Dist_M^exponent), 
+                       max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
   
-  lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
+  lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), 
+        unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
   
-  points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-  points((temporary$Dist_M[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+  points(temporary$Dist_M^exponent, 
+         temporary$Depth_mean, 
+         cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, 
+         pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+  points((temporary$Dist_M[temporary$cps.nasc / 50000 * 30 > 1])^exponent, 
+         temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], 
+         pch = ".", cex = 2, col = "red")
   
-  abline(h = 50, col = "gray", lwd = 0.5)
+  abline(h = 50,  col = "gray", lwd = 0.5)
   abline(h = 100, col = "gray", lwd = 0.5)
   abline(h = 150, col = "gray", lwd = 0.5)
   abline(h = 200, col = "gray", lwd = 0.5)
   abline(h = 250, col = "gray", lwd = 0.5)
   abline(h = 300, col = "gray", lwd = 0.5)
-  abline(h = 0, col = "gray", lwd = 0.5)
+  abline(h = 0,   col = "gray", lwd = 0.5)
   
   
   if (test.4 == 1) {
-    polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), c(new.lines.top$y, rev(new.lines.bottom$y)), col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
+    polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), 
+            c(new.lines.top$y, rev(new.lines.bottom$y)), 
+            col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
   }
-  points(temporary$Dist_M^exponent, temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
-  points(temporary$Dist_M^exponent, temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
+  points(temporary$Dist_M^exponent, 
+         temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
+  points(temporary$Dist_M^exponent, 
+         temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
   
-  if (jpeg == T) {
-    jpeg(paste(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_clean", ".jpeg", sep = ""), width = 35, height = 25, units = "cm", res = 200)
-    plot(temporary$Dist_M^exponent, temporary$Depth_mean, ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n")
-    axis(side = 1, at = seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5), lab = floor(seq(min(temporary$Dist_M^exponent), max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
+  if (jpeg) {
+    jpeg(paste0(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_clean.jpeg"), 
+         width = 35, height = 25, units = "cm", res = 200)
+    plot(temporary$Dist_M^exponent, 
+         temporary$Depth_mean, 
+         ylim = c(max.range, 0), type = "n", main = acoustic.file.list[test.1], 
+         ylab = "Depth (m)", xlab = "Dist_M", xaxt = "n")
+    axis(side = 1, at = seq(min(temporary$Dist_M^exponent), 
+                            max(temporary$Dist_M^exponent), l = 5), 
+         lab = floor(seq(min(temporary$Dist_M^exponent), 
+                         max(temporary$Dist_M^exponent), l = 5)^(1 / exponent)))
     
-    lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
+    lines(unlist(lapply(split(temporary$Dist_M^exponent, temporary$Dist_M), FUN = unique)), 
+          unlist(lapply(split(temporary$Depth_mean, temporary$Dist_M), FUN = max)), col = 1, lwd = 2)
     
-    points(temporary$Dist_M^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
-    points((temporary$Dist_M[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
+    points(temporary$Dist_M^exponent, 
+           temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, 
+           pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+    points((temporary$Dist_M[temporary$cps.nasc / 50000 * 30 > 1])^exponent, 
+           temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], 
+           pch = ".", cex = 2, col = "red")
     
-    abline(h = 50, col = "gray", lwd = 0.5)
+    abline(h = 50,  col = "gray", lwd = 0.5)
     abline(h = 100, col = "gray", lwd = 0.5)
     abline(h = 150, col = "gray", lwd = 0.5)
     abline(h = 200, col = "gray", lwd = 0.5)
     abline(h = 250, col = "gray", lwd = 0.5)
     abline(h = 300, col = "gray", lwd = 0.5)
-    abline(h = 0, col = "gray", lwd = 0.5)
+    abline(h = 0,   col = "gray", lwd = 0.5)
     
     if (test.4 == 1) {
-      polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), c(new.lines.top$y, rev(new.lines.bottom$y)), col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
+      polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), 
+              c(new.lines.top$y, rev(new.lines.bottom$y)), 
+              col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
     }
-    points(temporary$Dist_M^exponent, temporary$top.habitat, cex = 0.5, col = "red", pch = 19)
-    points(temporary$Dist_M^exponent, temporary$bottom.habitat, cex = 0.5, col = "blue", pch = 19)
-    dev.off()
+    points(temporary$Dist_M^exponent, 
+           temporary$top.habitat, 
+           cex = 0.5, col = "red", pch = 19)
+    points(temporary$Dist_M^exponent, 
+           temporary$bottom.habitat, 
+           cex = 0.5, col = "blue", pch = 19)
+    graphics.off()
   }
   
   write.csv(temporary,
@@ -217,4 +276,7 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, path.img = NULL,
             row.names = FALSE,
             quote = FALSE
   )
+  
+  # Close both graphics devices
+  graphics.off()
 }
--- Code/extract_CPS_NASC.R
+++ Code/extract_CPS_NASC.R
@@ -1,81 +1,106 @@
-# A utility for extracting CPS backscatter (cps.NASC)
+# A utility for extracting CPS backscatter (cps.nasc) from Echoview CSV files
+# Source the script and respond to the prompts
+# If a path to Exported Images is provided, corresponding echograms will 
+
 # Install and load pacman (library management package)
 if (!require("pacman")) install.packages("pacman")
 
 # Install and load required packages from CRAN ---------------------------------
-pacman::p_load(tidyverse, here,fs)
+pacman::p_load(tidyverse, here, fs)
 
 # Install and load required packages from Github -------------------------------
-## atm
 pacman::p_load_gh("kstierhoff/atm")
 
-# Load CTDapp function
+# Set theme for plotting
+theme_set(theme_bw())
+
+# Close any open graphics windows
+graphics.off()
+
+# Load function used to estimate CPS NASC --------------------------------------
 source(here::here("Code/estimate_CPS_NASC.R"))
 
-# Configure input and output paths
+# Configure input and output paths ---------------------------------------------
+## CSV input path (source)
 path.in  <- here("Data/Backscatter/RL")
+
+## CSV output path (destination) and new file suffix
 path.out <- here("Data/Backscatter/RL")
+suffix.out <- "_nasc_cps"
+
+## Path to echogram images exported from Echoview
 path.img <- "C:/SURVEY/2207RL/PROCESSED/EV/Exported_Images"
 
-# Run the function to estimate CPS backscatter
+# Run the function to estimate CPS backscatter --------------------------------
 estimate.cps.nasc(path.input = path.in, 
                   pattern = "CPS-Final 38 kHz CPS.csv", 
                   path.output = path.out, 
+                  suffix = suffix.out,
                   path.img = path.img,
                   pattern.img = "CPS-38 kHz CPS for Image Export.png",
-                  expand.right = T,
-                  expand.left = F,
-                  expansion = 2, max.range = 350, root = 2,  scaling = 0.5)
+                  expand.right = TRUE,
+                  expand.left = TRUE,
+                  expansion = 2, max.range = 350, root = 2,  scaling = 0.1,
+                  jpeg = FALSE)
 
-# Close both graphics devices
-dev.off()
-dev.off()
-
-# Recreate Juan's original bubble plots ----------------------------------------
+# Create final figure showing results of processing ---------------------------
 # List new nasc_cps.csv files
-list.files(path = path.out, pattern = "CPS-Final 38 kHz CPS_nasc_cps.csv", recursive = FALSE)
-
-masked.file.list <- list.files(path = path.out, pattern = "CPS-Final 38 kHz CPS_nasc_cps.csv", recursive = FALSE)
+masked.file.list <- list.files(path = path.out, pattern = suffix.out, recursive = FALSE)
 print(masked.file.list)
 
-print("Type the order of the file you want to integrate")
+# Ask for the nasc_cps.csv file to plot
+print("Type the order of the file you want to plot:")
 masked.file.num <- scan(n = 1)
 
-# Create file info
+# Create file info variables
 new.masked.filename <- masked.file.list[masked.file.num]
 new.masked.path <- file.path(path.out, new.masked.filename)
-new.masked.plot <- paste0(fs::path_ext_remove(fs::path_file(new.masked.path)), ".png")
+new.masked.plot <- paste0(path_ext_remove(path_file(new.masked.path)), ".png")
 
 # Read new nasc_cps.csv file
-new.masked.file <- read_csv(new.masked.path)
+new.masked.file <- read_csv(new.masked.path) %>% 
+  arrange(NASC)
 
 ## Summarize file for plotting the seabed depth
-max.layer.depth <- new.masked.file %>% 
+seabed.depth <- new.masked.file %>% 
   group_by(Dist_M) %>% 
   summarize(max.depth = -max(Depth_mean))
 
-## Create the bubble plot
+## Plot results
 cps.nasc.bubble <- ggplot(new.masked.file) +
-  geom_point(
-    data = filter(new.masked.file, NASC > 0),
-    aes(Dist_M, -Depth_mean, size = NASC),
-    shape = 21, fill = NA, alpha = 0.5, show.legend = FALSE) +
-  geom_point(
-    data = filter(new.masked.file, cps.nasc > 0),
-    aes(Dist_M, -Depth_mean, size = cps.nasc),
-    alpha = 0.5, colour = "blue", show.legend = FALSE ) +
-  geom_line(
-    data = filter(max.layer.depth),
-    aes(Dist_M, max.depth), alpha = 0.5 ) +
+  # Plot the seabed
+  geom_line(data = seabed.depth,
+            aes(Dist_M, max.depth), alpha = 0.5) +
+  # Plot the surface
+  geom_hline(yintercept = 1) +
+  # Plot the top habitat line
   geom_line(aes(Dist_M, -top.habitat), colour = "red", linetype = "dashed") +
+  # Plot the bottom habitat line
   geom_line(aes(Dist_M, -bottom.habitat), colour = "blue", linetype = "dashed") +
-  theme_bw() +
-  labs(
-    title = new.masked.filename,
-    x = "Echoview distance (M)", y = "Mean depth (m)")
+  # Plot NASC that was removed
+  geom_point(data = filter(new.masked.file, NASC != cps.nasc),
+    aes(Dist_M, -Depth_mean, size = NASC),
+    shape = 21, fill = NA, alpha = 0.8, show.legend = FALSE) +
+  # Plot NASC that was retained
+  geom_point(data = filter(new.masked.file, NASC > 0, NASC == cps.nasc),
+    aes(Dist_M, -Depth_mean, size = NASC, fill = NASC),
+    shape = 21, alpha = 0.9, show.legend = TRUE) +
+  # Configure axes and scales
+  scale_x_continuous(position = "top", breaks = seq(0, max(new.masked.file$Dist_M), 2000), expand = c(0,0)) +
+  scale_y_continuous(breaks = -rev(seq(0, signif(max(new.masked.file$Depth_mean), 1), 50))) +
+  scale_fill_viridis_c() +
+  # Configure labels and title
+  labs(title = new.masked.filename,
+       x = "Echoview distance (M)", 
+       y = "Mean depth (m)") + 
+  # Set themes
+  theme(axis.text.x = element_text(angle = 45, hjust = 0, vjust = 1.2))
 
 # Save the plot
 ggsave(cps.nasc.bubble,
        filename = file.path(path.out, new.masked.plot),
        width = 15, height = 7
 )
+
+# Open the newly created plot
+shell.exec(file.path(path.out, new.masked.plot))
