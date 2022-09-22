299a3f9c7886c3439dfc38c4a6ac5663853b0835
Kevin Stierhoff <kevins@ucsc.edu>
2022-09-21 17:08:40 -0700

    Added ggplot of masked CPS nasc data from extract_cps_nasc.r.

--- Code/estimate_CPS_NASC.R
+++ Code/estimate_CPS_NASC.R
@@ -1,5 +1,6 @@
-estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200, transparency = 0.2, 
-                              expand.left = F, expand.right = F, expansion = 2, suffix = "_nasc_cps", jpeg = T, root =1.5, scaling = 1) {
+estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200, transparency = 0.2,
+                              expand.left = F, expand.right = F, expansion = 2, suffix = "_nasc_cps", 
+                              jpeg = T, root = 1.5, scaling = 1) {
   exponent <- 1
   
   if (expand.left == T) {
@@ -19,13 +20,16 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   for (i in acoustic.file.list[test.1]) {
     print(acoustic.file.list[test.1])
     
-    processed.file <- list.files(path = path.input, 
-                                 pattern =  
-                                   paste( substr(acoustic.file.list[test.1],1, nchar(acoustic.file.list[test.1])-4 )   , "_nasc_cps",sep ="")
-                                 , recursive = F)
+    processed.file <- list.files(
+      path = path.input,
+      pattern =
+        paste(substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_nasc_cps", sep = ""),
+      recursive = F
+    )
     
-    if(length(processed.file)>0)
+    if (length(processed.file) > 0) {
       print(paste("There is already a processed file with the name", processed.file))
+    }
     
     
     print("Is this the file you want to integrate? 1 = Yes; 0 = No")
@@ -36,13 +40,16 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
       print("Chose another file")
       test.1 <- scan(n = 1)
       print(acoustic.file.list[test.1])
-      processed.file <- list.files(path = path.input, 
-                                   pattern =  
-                                     paste( substr(acoustic.file.list[test.1],1, nchar(acoustic.file.list[test.1])-4 )   , "_nasc_cps",sep ="")
-                                   , recursive = F)
+      processed.file <- list.files(
+        path = path.input,
+        pattern =
+          paste(substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), "_nasc_cps", sep = ""),
+        recursive = F
+      )
       
-      if(length(processed.file)>0)
+      if (length(processed.file) > 0) {
         print(paste("There is already a processed file with the name", processed.file))
+      }
       print("Is this the file you want to integrate? 1 = Yes; 0 = No")
       test.2 <- scan(n = 1)
     }
@@ -51,8 +58,9 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   
   print(paste("Expansion =", exponent))
   
-  temporary <- read.csv(paste(path.input, "\\", acoustic.file.list[test.1], sep = ""), 
-                        fileEncoding = "UTF-8-BOM")
+  temporary <- read.csv(paste(path.input, "\\", acoustic.file.list[test.1], sep = ""),
+                        fileEncoding = "UTF-8-BOM"
+  )
   
   # plot
   if (jpeg == T) {
@@ -64,7 +72,7 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
     
     lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
     
-    points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1/root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+    points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
     points((temporary$Interval[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
     
     abline(h = 50, col = "gray", lwd = 0.5)
@@ -85,7 +93,7 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   
   lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
   
-  points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1/root)*30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+  points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$NASC / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
   points((temporary$Interval[temporary$NASC / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$NASC / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
   
   abline(h = 50, col = "gray", lwd = 0.5)
@@ -148,7 +156,7 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
   
   lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
   
-  points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1/root)*30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+  points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
   points((temporary$Interval[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
   
   abline(h = 50, col = "gray", lwd = 0.5)
@@ -173,7 +181,7 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
     
     lines(unlist(lapply(split(temporary$Interval^exponent, temporary$Interval), FUN = unique)), unlist(lapply(split(temporary$Depth_mean, temporary$Interval), FUN = max)), col = 1, lwd = 2)
     
-    points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1/root)*30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
+    points(temporary$Interval^exponent, temporary$Depth_mean, cex = (temporary$cps.nasc / (50000 / scaling))^(1 / root) * 30, pch = 19, col = rgb(t(col2rgb("blue")) / 255, alpha = transparency))
     points((temporary$Interval[temporary$cps.nasc / 50000 * 30 > 1])^exponent, temporary$Depth_mean[temporary$cps.nasc / 50000 * 30 > 1], pch = ".", cex = 2, col = "red")
     
     abline(h = 50, col = "gray", lwd = 0.5)
@@ -184,7 +192,6 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
     abline(h = 300, col = "gray", lwd = 0.5)
     abline(h = 0, col = "gray", lwd = 0.5)
     
-    
     if (test.4 == 1) {
       polygon(c(new.lines.top$x, rev(new.lines.bottom$x)), c(new.lines.top$y, rev(new.lines.bottom$y)), col = rgb(t(col2rgb("yellow")) / 255, alpha = 0.15), border = NA)
     }
@@ -193,8 +200,9 @@ estimate.cps.nasc <- function(path.input, pattern, path.output, max.range = 200,
     dev.off()
   }
   
-  write.csv(temporary, 
+  write.csv(temporary,
             paste0(path.output, "\\", substr(acoustic.file.list[test.1], 1, nchar(acoustic.file.list[test.1]) - 4), suffix, ".csv"),
             row.names = FALSE,
-            quote = FALSE)
+            quote = FALSE
+  )
 }
--- Code/extract_CPS_NASC.R
+++ Code/extract_CPS_NASC.R
@@ -13,12 +13,12 @@ pacman::p_load_gh("kstierhoff/atm")
 source(here::here("Code/estimate_CPS_NASC.R"))
 
 # Configure input and output paths
-# path.in  <- here("Data/Backscatter/SD")
-# path.out <- here("Data/Backscatter/SD")
+path.in  <- here("Data/Backscatter/RL")
+path.out <- here("Data/Backscatter/RL")
 # path.in  <- "C:/SURVEY/2107RL/PROCESSED/EV/CSV/SAILDRONE/SD1059"
 # path.out <- "C:/SURVEY/2107RL/PROCESSED/EV/CSV/SAILDRONE/SD1059"
-path.in  <- "C:/Users/josiah.renfree/Desktop/2107RL_SD_Reprocessing/EV/CSV/SD1059/CPSFilter_-13dB"
-path.out <- "C:/Users/josiah.renfree/Desktop/2107RL_SD_Reprocessing/EV/CSV/SD1059/CPSFilter_-13dB/POST-PROCESSED"
+# path.in  <- "C:/Users/josiah.renfree/Desktop/2107RL_SD_Reprocessing/EV/CSV/SD1059/CPSFilter_-13dB"
+# path.out <- "C:/Users/josiah.renfree/Desktop/2107RL_SD_Reprocessing/EV/CSV/SD1059/CPSFilter_-13dB/POST-PROCESSED"
 
 # List CSV files (this is just for viewing the files; it's not used in the code)
 list.files(path = path.in, pattern = "CPS-Final 38 kHz CPS.csv", recursive = FALSE)
@@ -34,3 +34,50 @@ estimate.cps.nasc(path.input = path.in,
 # Close both graphics devices
 dev.off()
 dev.off()
+
+# Recreate Juan's original bubble plots ----------------------------------------
+# List new nasc_cps.csv files
+list.files(path = path.out, pattern = "CPS-Final 38 kHz CPS_nasc_cps.csv", recursive = FALSE)
+
+masked.file.list <- list.files(path = path.out, pattern = "CPS-Final 38 kHz CPS_nasc_cps.csv", recursive = FALSE)
+print(masked.file.list)
+
+print("Type the order of the file you want to integrate")
+masked.file.num <- scan(n = 1)
+
+# Create file info
+new.masked.filename <- masked.file.list[masked.file.num]
+new.masked.path <- file.path(path.out, new.masked.filename)
+new.masked.plot <- paste0(fs::path_ext_remove(fs::path_file(new.masked.path)), ".png")
+
+# Read new nasc_cps.csv file
+new.masked.file <- read_csv(new.masked.path)
+
+## Summarize file for plotting the seabed depth
+max.layer.depth <- new.masked.file %>% 
+  group_by(Dist_M) %>% 
+  summarize(max.depth = -max(Depth_mean))
+
+## Create the bubble plot
+cps.nasc.bubble <- ggplot(new.masked.file) +
+  geom_point(
+    data = filter(new.masked.file, NASC > 0),
+    aes(Dist_M, -Depth_mean, size = NASC),
+    shape = 21, fill = NA, alpha = 0.5, show.legend = FALSE) +
+  geom_point(
+    data = filter(new.masked.file, cps.nasc > 0),
+    aes(Dist_M, -Depth_mean, size = cps.nasc),
+    alpha = 0.5, colour = "blue", show.legend = FALSE ) +
+  geom_line(
+    data = filter(max.layer.depth),
+    aes(Dist_M, max.depth), alpha = 0.5 ) +
+  geom_line(aes(Dist_M, -top.habitat), colour = "red", linetype = "dashed") +
+  geom_line(aes(Dist_M, -bottom.habitat), colour = "blue", linetype = "dashed") +
+  theme_bw() +
+  labs(x = "Echoview distance (M)", y = "Mean depth (m)")
+
+# Save the plot
+ggsave(cps.nasc.bubble,
+       filename = file.path(path.out, new.masked.plot),
+       width = 15, height = 7
+)
