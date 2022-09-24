8c3d7f73a2d530ec5a98213f75a97d07cbe679b3
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2022-09-24 15:03:46 +0000

    Cleaned up the interface to extract_CPS_NASC.r.

--- Code/extract_CPS_NASC.R
+++ Code/extract_CPS_NASC.R
@@ -1,13 +1,13 @@
 # A utility for extracting CPS backscatter (cps.nasc) from Echoview CSV files
-# Source the script and respond to the prompts
-# If a path to Exported Images is provided, corresponding echograms will 
+# Source the script (Ctrl+Shift+S) and respond to the prompts
+# If a path to Exported Images is provided (path.img), 
+# corresponding echograms will be displayed
 
 # Install and load pacman (library management package)
 if (!require("pacman")) install.packages("pacman")
 
 # Install and load required packages from CRAN ---------------------------------
 pacman::p_load(tidyverse, here, fs)
-
 # Install and load required packages from Github -------------------------------
 pacman::p_load_gh("kstierhoff/atm")
 
@@ -17,30 +17,25 @@ graphics.off()
 # Load function used to estimate CPS NASC --------------------------------------
 source(here::here("Code/estimate_CPS_NASC.R"))
 
-# Configure input and output paths ---------------------------------------------
-## CSV input path (source)
-path.in  <- here("Data/Backscatter/RL")
-# path.in <- "C:/SURVEY/2207RL/PROCESSED/EV/CSV/LASKER"
+# Extract only CPS backscatter -------------------------------------------------
+extractNASC(
+  # Most used options ----------------------------------------------------------
+  path.in     = "C:/SURVEY/2207RL/PROCESSED/EV/CSV/LASKER", # CSV file source                                                                                                 e
+  pattern.in  = "_CPS-Final 38 kHz CPS.csv", # CSV file regex
+  path.out    = here("Data/Backscatter/RL"), # Processed file destination
+  suffix.out  = "_nasc_cps.csv",             # Suffix applied to processed CSV files
+  path.img    = "C:/SURVEY/2207RL/PROCESSED/EV/Exported_Images", # Location of exported image files, or NULL
+  pattern.img = "_CPS-38 kHz CPS for Image Export.png", # Exported image regex
+  # Lesser used options --------------------------------------------------------
+  expansion   = 2,     # Constant for expanding axes
+  max.range   = 250,   # Depth range for bubble plots
+  root        = 2,     # Constant for controlling bubble size (2)
+  scaling     = 0.1,   # Constant for controlling bubble size (0.1)
+  jpeg        = FALSE, # Save intermediate plots from line picks
+  x11.w       = 1600,  # Width of graphics window (px)
+  x11.h       = 600)   # Height of graphics window (px)
 
-## CSV output path (destination)
-path.out <- here("Data/Backscatter/RL")
-
-## Path to echogram images exported from Echoview
-path.img <- "C:/SURVEY/2207RL/PROCESSED/EV/Exported_Images"
-
-# Run the function to estimate CPS backscatter --------------------------------
-extractNASC(path.in = path.in, # CSV file source                                                                                                 e
-            pattern.in = "_CPS-Final 38 kHz CPS.csv", # CSV file regex
-            path.out = path.out, # Processed file destination
-            suffix.out = "_nasc_cps.csv", # Suffix applied to processed CSV files
-            path.img = path.img,  # Location of exported image files
-            pattern.img = "_CPS-38 kHz CPS for Image Export.png", # Exported image regex
-            # expand.right = F,    # Expand right side of plot
-            # expand.left  = T,    # Expand left side of plot
-            expansion = 2,       # Constant for expanding axes
-            max.range = 250,     # Depth range for bubble plots
-            root = 2,            # Constant for controlling bubble size (2)
-            scaling = 0.1,       # Constant for controlling bubble size (0.1)
-            jpeg = FALSE,        # Save intermediate plots from line picks
-            x11.w = 1600,        # Width of graphics window (px)
-            x11.h = 600)         # Height of graphics window (px)
+# # Configure input and output paths ---------------------------------------------
+# ## CSV input path (source)
+# path.in  <- here("Data/Backscatter/RL") # KLS laptop
+# # path.in <- "C:/SURVEY/2207RL/PROCESSED/EV/CSV/LASKER" # Echoview PC
