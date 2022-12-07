36ae4ab0ac04d309d100d32d10af74a851159e2d
Kevin Stierhoff <kevins@ucsc.edu>
2022-12-07 17:28:06 +0000

    Minor edits to estimateBiomass to finalize preliminary biomass estimates. Added output_2207RL.R and presentBiomass_2207RL.

--- Doc/settings/settings_2207RL.R
+++ Doc/settings/settings_2207RL.R
@@ -560,7 +560,7 @@ bootstrap.est.spp      <- c("Clupea pallasii","Engraulis mordax","Sardinops saga
                             "Scomber japonicus","Trachurus symmetricus")
 
 # Number of bootstrap samples
-boot.num <- 5 # 1000 during final
+boot.num <- 1000 # 1000 during final
 
 # Generate biomass length frequencies
 do.lf    <- TRUE
