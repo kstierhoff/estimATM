c7e114227d8c00a4601bd0db03328c6254e4c864
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2025-02-19 08:55:13 -0800

    Added some files to begin developing checkSeines, akin to checkTrawls.

---
+++ Code/collect_seine_database.R
@@ -0,0 +1,48 @@
+# Collect seine data from the trawl database. 
+
+# Extracts all data in all specified tables and saves to an output file
+# Seine data are further processed and formatted in Code/format_seine_database.R
+# Intended to be run in scripts that load settings from Doc/settings
+
+if (seine.source == "SQL") {
+  # Configure ODBC connection to TRAWL database
+  seine.con  <- DBI::dbConnect(odbc::odbc(), 
+                          Driver = "SQL Server", 
+                          Server = "161.55.235.187", 
+                          Database = "Trawl_Dev", 
+                          Trusted_Connection = "True")
+} else if (seine.source == "Access") {
+  # Copy trawl Access database
+  haul.db <- fs::dir_ls(file.path(survey.dir[survey.vessel.primary],
+                              trawl.dir.access),
+                    regexp = trawl.db.access)
+  
+  fs::file_copy(haul.db, here::here("Data/Trawl"), overwrite = TRUE)
+  
+  # Configure ODBC connection to TRAWL database
+  seine.con  <- DBI::dbConnect(odbc::odbc(), 
+                          Driver = "Microsoft Access Driver (*.mdb, *.accdb)", 
+                          DBQ = file.path(here::here("Data/Trawl"), trawl.db.access))
+}
+# Import trawl database tables
+catch.all	     <- dplyr::tbl(seine.con,"Catch") %>% dplyr::collect()
+sets.all       <- dplyr::tbl(seine.con,"Haul") %>% dplyr::collect()
+lengths.all    <- dplyr::tbl(seine.con,"Specimen") %>% dplyr::collect()
+if (DBI::dbExistsTable(seine.con, "LengthFrequency"))
+  lengthFreq.all <- dplyr::tbl(seine.con,"LengthFrequency") %>% dplyr::collect()
+spp.codes      <- dplyr::tbl(seine.con,"SpeciesCodes") %>% dplyr::collect()
+
+# Close database channel
+DBI::dbDisconnect(seine.con)
+
+# Create directory for saving seine data
+fs::dir_create(here("Data/Seine"))
+
+# Save imported database data to .Rdata file
+if (exists("lengthFreq.all")) {
+  save(catch.all, sets.all, lengths.all, spp.codes, lengthFreq.all, 
+       file = here::here("Data/Seine/seine_data_raw.Rdata"))
+} else {
+  save(catch.all, sets.all, lengths.all, spp.codes,  
+       file = here::here("Data/Seine/seine_data_raw.Rdata"))
+}
